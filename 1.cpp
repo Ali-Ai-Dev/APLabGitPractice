de95423c4e7d606fce14febd0020304ab8706295
BorhanM <b.moyassari@ec.iut.ac.ir>
2023-05-15 14:28:44 +0330

    File 1 bugs fixed

--- 1.cpp
+++ 1.cpp
@@ -3,38 +3,41 @@ using namespace std;
 
 class container {
 
-	int size;
+    int size;
 public:
-	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
+    float* p;
+    container(int s) :size(s){}
+    int& getsize() { return size;}
 
 };
 
 class vector :public container {
 
-	int call_num;
+    int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
-		p = new float();
-	}
-	int len;
-	int& getlen() const {
-		call_num ++;
-		return len;
-	}
-	~vector() = default;
+    int len;
+    explicit vector( int l) : container(100), len(l) {
+        p = new float();
+    }
+    int& getlen() {
+        call_num ++;
+        return len;
+    }
+    ~vector() = default;
+
+
 };
 
 int main() {
 
-	container c1(100);
-	vector v1 = c1;
-	container& r1 = v1;
-	container c2 = 100;
-	c2.getsize() = 20;
-	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
-	cout << v2.getlen();
+    container c1(100);
+    vector v1(100);
+    container& r1 = v1;
+    container c2 = 100;
+    c2.getsize() = 20;
+    cout << c2.getsize();
+    vector v2(100);
+    v2.getlen() = 40;
+    cout << v2.getlen();
+
 }
