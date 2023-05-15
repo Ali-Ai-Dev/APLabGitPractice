5c2a66c5467cffafb05c991704ece673ab4e7c30
Borhan <bmoyassari@gmail.com>
2023-05-15 16:15:08 +0330

    File 4 fixed

--- 4.cpp
+++ 4.cpp
@@ -2,8 +2,8 @@
 int main()
 {
     float arr[5] = { 12.5, 10.0, 13.5, 90.5, 0.5 };
-    float *ptr1 = &arr[0];
-    float *ptr2 = ptr1 + 3;
-    printf("%f", *ptr2 - *ptr1);
+    float ptr1 = arr[0];
+    float ptr2 = ptr1 + 3;
+    printf("%f", ptr2 - ptr1);
     return 0;
 }
