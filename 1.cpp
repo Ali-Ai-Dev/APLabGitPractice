5eb4ebea0baf0f38d1f65a6d03842dccda1632ee
Seyed <sm.tp.1382@gmail.com>
2023-05-16 16:50:19 +0330

    debugged 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -15,12 +15,11 @@ class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) :len(l),container(1 * 100){
 		p = new float();
 	}
 	int len;
-	int& getlen() const {
-		call_num ++;
+	const int& getlen() const {
 		return len;
 	}
 	~vector() = default;
@@ -29,12 +28,12 @@ public:
 int main() {
 
 	container c1(100);
-	vector v1 = c1;
+	vector v1(10);
 	container& r1 = v1;
 	container c2 = 100;
-	c2.getsize() = 20;
+	c2.getsize();
 	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	vector v2(100);
+	//v2.getlen = 40; ////this dosn't work because type of function is 'const int'
 	cout << v2.getlen();
 }
