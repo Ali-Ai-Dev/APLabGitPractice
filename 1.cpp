834f126892434aaab9eb350f0d9660018e779b2d
Mahan <mahan.beidokhti1382@gmail.com>
2023-05-16 23:23:19 +0330

    1.cpp corrected!

--- 1.cpp
+++ 1.cpp
@@ -1,40 +1,59 @@
 #include <iostream>
 using namespace std;
 
-class container {
-
+class container
+{
+protected:
 	int size;
-public:
-	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
+	float *p;
 
+public:
+	container() {}
+
+	container(int s) : size(s) {}
+	int &getsize() { return size; }
 };
 
-class vector :public container {
+class vector : public container
+{
 
 	int call_num;
+	int len;
+
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	vector(int l)
+	{
+		len = l;
+		size = 100;
 		p = new float();
 	}
-	int len;
-	int& getlen() const {
-		call_num ++;
+	vector(container temp)
+	{
+		this->len = temp.getsize();
+		call_num = 1;
+		return;
+	}
+	int &getlen()
+	{
+		call_num++;
 		return len;
 	}
 	~vector() = default;
 };
 
-int main() {
+int main()
+{
 
 	container c1(100);
 	vector v1 = c1;
-	container& r1 = v1;
+	container &r1 = v1;
 	container c2 = 100;
 	c2.getsize() = 20;
 	cout << c2.getsize();
 	vector v2 = 100;
-	v2.getlen = 40;
+	v2.getlen() = 40;
 	cout << v2.getlen();
 }
+
+// "const" removed // "explicit" removed // "int len;" moved up // "len" and "size" constructed // constructor with container type created // const of getlen removed 
+// output : 2040
