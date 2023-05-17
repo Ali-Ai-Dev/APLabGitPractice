902317538531250806fe22d25e9634a400b44ac9
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-05-17 17:19:47 +0330

    printing 4.cpp output

--- 4.cpp
+++ 4.cpp
@@ -1,9 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    float arr[5] = { 12.5, 10.0, 13.5, 90.5, 0.5 };
+    float arr[5] = {12.5, 10.0, 13.5, 90.5, 0.5};
     float *ptr1 = &arr[0];
     float *ptr2 = ptr1 + 3;
     printf("%f", *ptr2 - *ptr1);
     return 0;
 }
+
+//output : 78.000000
