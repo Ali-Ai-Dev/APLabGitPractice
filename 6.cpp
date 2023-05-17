951e5c965413693e0875919b9459d31144834f8d
Xmojtabw <xmojatbw@gmail.com>
2023-05-18 00:57:46 +0330

    6: added comments

--- 6.cpp
+++ 6.cpp
@@ -1,11 +1,12 @@
-﻿#include<stdio.h>
+﻿#include <stdio.h>
 int main()
 {
 	int a;
 	char *x;
-	x = (char *)&a;
-	a = 512;
-	x[0] = 1;
+	x = (char *)&a; // changing int* to char*
+	a = 512;		// a will look like this:0000 0010 0000 0000
+	//       x looks like an array like this:x[3] x[2] x[1] x[0]
+	x[0] = 1; // now look like               0000 0010 0000 0001
 	printf("%d\n", a);
 	return 0;
 }
