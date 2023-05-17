bfe844d6c582424d7d924f704d697a1185f40f75
MahsaTK <mtk.mahsa04@gmail.com>
2023-05-18 00:02:40 +0330

    50 2

--- 5.cpp
+++ 5.cpp
@@ -2,9 +2,11 @@
 int main()
 {
     int arr[] = { 10, 20, 30, 40, 50, 60 };
-    int *ptr1 = arr;
-    int *ptr2 = arr + 5;
+    int* ptr1 = arr;
+    int* ptr2 = arr + 5;
     printf("%d\n", (*ptr2 - *ptr1));
     printf("%c", (char)(*ptr2 - *ptr1));
     return 0;
 }
+//50
+//2
