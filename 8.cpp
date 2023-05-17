39501c85eccc193e0b022148ee64862715c6e335
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-05-17 13:26:25 +0330

    added stdlib for sizeof and auto

--- 8.cpp
+++ 8.cpp
@@ -1,13 +1,13 @@
-﻿#include<stdio.h>
-const char * f(const char **p) {
+﻿#include <stdio.h>
+#include <stdlib.h> //added stdlib for sizeof and auto
+const char *f(const char **p)
+{
 	auto q = (p + sizeof(char))[1];
 	return q;
 }
-int main() {
-	const char * str[] = { "Wish","You","Best",":D" };
+int main()
+{
+	const char *str[] = {"Wish", "You", "Best", ":D"};
 	printf("%c%c ", *f(str), *(f(str) + 1));
 	printf("%c%c%c%c\n", **str, *(*(str + 1) + 1), *((str + 2)[-1] + 1), **&*(&str[-1] + 1));
-
-
-	
 }
