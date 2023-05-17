ad08181aa715b1a183054364b92a0e0f4c20a037
NiMaRhM <nimahanexrahimi123@gmail.com>
2023-05-18 01:08:06 +0330

    Bug Fixed

--- 1.cpp
+++ 1.cpp
@@ -1,40 +1,60 @@
 #include <iostream>
+
 using namespace std;
 
 class container {
-
-	int size;
+protected:
+    int size;
 public:
-	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
+    float *p;
 
+    container(int s) : size(s) {}
+
+    container() = default;
+
+    const int &getsize() { return size; }
+
+    void setSize(int x) { size = x ;}
 };
 
-class vector :public container {
-
-	int call_num;
+class vector : public container {
+    int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
-		p = new float();
-	}
-	int len;
-	int& getlen() const {
-		call_num ++;
-		return len;
-	}
-	~vector() = default;
+    explicit vector(int l) : len(l) {
+        size = 1 * 100;
+        p = new float();
+    }
+
+    vector &operator=(container x) {
+        vector y(1);
+        y.setSize(x.getsize());
+        y.p = x.p;
+        y.len = 1;
+        return y;
+    }
+
+    vector() = default;
+
+    int len;
+
+    int getlen() {
+        call_num++;
+        return len;
+    }
+
+    ~vector() = default;
 };
 
 int main() {
 
-	container c1(100);
-	vector v1 = c1;
-	container& r1 = v1;
-	container c2 = 100;
-	c2.getsize() = 20;
-	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
-	cout << v2.getlen();
+    container c1(100);
+    vector v1;
+    v1 = c1;
+    container &r1 = v1;
+    container c2 = 100;
+    c2.setSize(20);
+    cout << c2.getsize();
+    vector v2 (100);
+    v2.len = 40;
+    cout << v2.getlen();
 }
