d648c747de17f5d3a7e0c7e056ecddf74064f49f
kimnik00 <kimia.nikoee1383@gmail.com>
2023-05-19 18:44:06 +0430

    code is fixed

--- 1.cpp
+++ 1.cpp
@@ -6,8 +6,9 @@ class container {
 	int size;
 public:
 	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
+	container() {}
+	container(int s) :size(s) {}
+	int& getsize() { return size; }
 
 };
 
@@ -15,26 +16,41 @@ class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) :len(l), conainer(1 * 100) {
+		call_num = 0;
 		p = new float();
 	}
+	vector(container c) :len(l), conainer(c, getsize()) {
+		call_num = 0;
+		p = new float();
+	}
+
 	int len;
-	int& getlen() const {
-		call_num ++;
+	int& getlen() {
+		call_num++;
 		return len;
 	}
-	~vector() = default;
+	vector& operator=(container c) : len(l), container(c, getsize()) {
+		call_num = 0;
+		p = new float();
+		return *this;
+	}
+	~vector() {
+
+		delete p;
+	}
 };
 
 int main() {
 
+
 	container c1(100);
 	vector v1 = c1;
 	container& r1 = v1;
-	container c2 = 100;
+	container c2(100);
 	c2.getsize() = 20;
-	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	cout << c2.getsize() << endl;
+	vector v2(100);
+	v2.getlen() = 40;
 	cout << v2.getlen();
-}
+}}
