acae3bb57d64a474b02a0553f87153ae67430463
SINA <sina.jahangir.013@gmail.com>
2023-05-20 21:14:16 +0330

    1.cpp is debugged

--- 1.cpp
+++ 1.cpp
@@ -7,7 +7,7 @@ class container {
 public:
 	float* p;
 	container(int s) :size(s){}
-	const int& getsize() { return size;}
+	int& getsize() { return size;}
 
 };
 
@@ -15,12 +15,20 @@ class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) :len(l) , container(1*100)
+	{
 		p = new float();
 	}
 	int len;
-	int& getlen() const {
-		call_num ++;
+	void set_call(int input) { call_num = input; }
+	vector(container input) : container(input.getsize())
+	{
+		this->p = input.p;
+		len = 0;
+		set_call(0);
+	}
+	int& getlen(){
+		call_num++;
 		return len;
 	}
 	~vector() = default;
@@ -34,7 +42,7 @@ int main() {
 	container c2 = 100;
 	c2.getsize() = 20;
 	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	vector v2 = vector(100);
+	v2.getlen() = 40;
 	cout << v2.getlen();
 }
